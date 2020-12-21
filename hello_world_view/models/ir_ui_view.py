f50898e379592aaed5f223e09468a5788125bd22
Sambat Lim <sambatlim86@gmail.com>
2020-12-21 10:02:57 +0700

    added ondelete method to fields.selection.

--- hello_world_view/models/ir_ui_view.py
+++ hello_world_view/models/ir_ui_view.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class View(models.Model):
     _inherit = 'ir.ui.view'
 
-    type = fields.Selection(selection_add=[('hello_world', "Hello World")])
+    type = fields.Selection(selection_add=[('hello_world', "Hello World")], ondelete={'hello_world': 'cascade'})
